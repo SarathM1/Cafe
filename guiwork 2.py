75abf8030287cb353938b2a2512905f2b69a9c59
Sarath M <sarathm00@gmail.com>
2015-08-08 00:05:30 +0530

    Created function setLabel to change label

--- guiwork 2.py
+++ guiwork 2.py
@@ -4,7 +4,7 @@ import socket
 
 class ping():
 	"""To ping computers"""
-	def __init__(self, ip,name, status="off"):
+	def __init__(self, ip,name, movex , movey , obj ,status="off"):
 		self.ip = ip
 		self.name = name
 
@@ -12,6 +12,7 @@ class ping():
 		self.s.settimeout (0.25)
 
 		self.img = self.name + status + '.png'
+		
 	
 	def ping(self):
 	    try:
@@ -20,12 +21,24 @@ class ping():
 	    	self.setImage('off')
 	    else:
 	        self.setImage('on')
+		self.s.close()
+		self.setLabel()
 	
+	def setLabel(self):
+		pixmap = QtGui.QPixmap(self.img)
+        self.lbl = QtGui.QLabel(obj)
+        self.lbl.setPixmap(pixmap)
+        #hbox.addWidget(self.lbl)
+        #self.setLayout(hbox)
+        self.lbl.move(movex,movey)
+        self.lbl.resize(120,70)
+
+
 	def setImage(self,st):
 		self.img = self.name + st + '.png'
 
 
-    s.close()	
+    	
 class Window(QtGui.QMainWindow):
 
     def __init__(self):
@@ -66,38 +79,38 @@ class Window(QtGui.QMainWindow):
 
         s.close()
 #25
-        pc1 = ping("192.168.0.172",'pc1')
+        pc1 = ping("192.168.0.172",50,180, self,'pc1')
         
 #24
-        pc2 = ping("192.168.0.127","pc2")
+        pc2 = ping("192.168.0.127",50,270,self,"pc2")
         
 #23
-        pc3 = ping("192.168.0.38","pc3")
+        pc3 = ping("192.168.0.38",50,360,self,"pc3")
 
 #22
-        pc4 = ping("192.168.0.114","pc4")
+        pc4 = ping("192.168.0.114",200,90,self,"pc4")
         
 #21
-        pc5 = ping("192.168.0.128","pc5")
+        pc5 = ping("192.168.0.128",200,180,self,"pc5")
         
 #20
-        pc6 = ping("192.168.0.20","pc6")
+        pc6 = ping("192.168.0.20",280,270,self,"pc6")
         
 #19
-        pc7 = ping("192.168.0.152","pc7")
+        pc7 = ping("192.168.0.152",280,360,self,"pc7")
         
 #18
-        pc8 = ping("192.168.0.153","pc8")
+        pc8 = ping("192.168.0.153",350,90,self,"pc8")
         
 #17
-        pc9 = ping("192.168.0.2","pc9")
+        pc9 = ping("192.168.0.2",350,180,self,"pc9")
 
 
 #16
-        pc10 = ping("192.168.0.128","pc10")
+        pc10 = ping("192.168.0.128",350,270,self,"pc10")
 
 #16
-        pc11 = ping("192.168.0.128","pc11")
+        pc11 = ping("192.168.0.128",350,360,self,"pc11")
         
 
 
@@ -115,99 +128,7 @@ class Window(QtGui.QMainWindow):
         lbl0.move(50,90)
         lbl0.resize(120,70)
 
-        pixmap = QtGui.QPixmap(pc3.img)
-        lbl1 = QtGui.QLabel(self)
-        lbl1.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl1.move(50,180)
-        lbl1.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc6.img)
-        lbl2 = QtGui.QLabel(self)
-        lbl2.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl2.move(50,270)
-        lbl2.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc9.img)
-        lbl3 = QtGui.QLabel(self)
-        lbl3.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl3.move(50,360)
-        lbl3.resize(120,70)
         
-## second coloumn
-
-        pixmap = QtGui.QPixmap(pc1.img)
-        lbl4 = QtGui.QLabel(self)
-        lbl4.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl4.move(200,90)
-        lbl4.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc4.img)
-        lbl5 = QtGui.QLabel(self)
-        lbl5.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl5.move(200,180)
-        lbl5.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc7.img)
-        lbl6 = QtGui.QLabel(self)
-        lbl6.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl6.move(200,270)
-        lbl6.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc10.img)
-        lbl7 = QtGui.QLabel(self)
-        lbl7.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl7.move(200,360)
-        lbl7.resize(120,70)
-
-## Third coloumn
-
-        pixmap = QtGui.QPixmap(pc2.img)
-        lbl8 = QtGui.QLabel(self)
-        lbl8.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl8.move(350,90)
-        lbl8.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc5.img)
-        lbl9 = QtGui.QLabel(self)
-        lbl9.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl9.move(350,180)
-        lbl9.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc8.img)
-        lbl10 = QtGui.QLabel(self)
-        lbl10.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl10.move(350,270)
-        lbl10.resize(120,70)
-
-        pixmap = QtGui.QPixmap(pc11.img)
-        lbl11 = QtGui.QLabel(self)
-        lbl11.setPixmap(pixmap)
-        #hbox.addWidget(lbl)
-        #self.setLayout(hbox)
-        lbl11.move(350,360)
-        lbl11.resize(120,70)
-
-
 
 
 
