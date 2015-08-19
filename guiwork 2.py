efe269c7538dc04c08518ae98e150d4a83a4e74d
Sarath M <sarathm00@gmail.com>
2015-08-19 15:42:22 +0530

    Debugging

--- guiwork 2.py
+++ guiwork 2.py
@@ -1,26 +1,39 @@
 import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore as qtcore
+from PyQt4.QtCore import pyqtSignal
 import socket
 import time
 
 class worker(qtcore.QThread):
-	def __init__(self):
+	def __init__(self,ip,name):
 		qtcore.QThread.__init__(self, parent=app)
 		self.signal = qtcore.SIGNAL("signal")
+		self.ip = ip
+		self.name = name
+		self.s =  socket.socket ()
+		#self.s.settimeout (0.25)
 	def run(self):
-		time.sleep(2)
-		print ("in thread")
-		self.emit(self.signal, "hi from thread")
+		#time.sleep(2)
+		#print ("in thread")
+		#self.emit(self.signal, "hi from thread")
+		print ('Starting '+str(self.name)+' '+str(self.ip))
+		try:
+			self.s.connect ((self.ip, 135))
+		except socket.error as e:
+			#self.setImage('off')
+			print('run: '+str(self.name)+' '+str(self.ip)+', '+str(e))
+			self.emit(self.signal, self.name+"off.png")
+		else:
+			#self.setImage('on')
+			self.emit(self.signal, self.name+"on.png")
 		
 class Window(QtGui.QMainWindow):
 
 	def __init__(self):
 		super(Window, self).__init__()  # SELF CAN REPLACE THE COMMAND WINDOW INSIDE CLASS WINDOW. EG. SELF.SHOW()  for   WINDOW.SHOW()
 
-		thread = worker()
-		self.connect(thread, thread.signal, self.testfunc)
-		thread.start()
+		
 
 		self.setGeometry(200,100, 900, 600)  # WINDOW SIZE
 		self.setWindowTitle("Radha Communications")   # WINDOW TITLE
@@ -53,9 +66,29 @@ class Window(QtGui.QMainWindow):
 		##  pc
 		
 		#-*while True:
-		pc0 = self.ping("192.168.1.19","pc0",50,90, self)
-#25
-		pc1 = self.ping("192.168.0.172","pc1",200,90, self)
+		
+		pc0 = self.ping("192.168.1.10","pc0",50,90, self)
+		thread = worker("192.168.1.10","pc0")
+		self.connect(thread, thread.signal, self.testfunc)
+		thread.start()
+		
+		"""
+		pixmap = QtGui.QPixmap(self.img)
+		print ("pc0 img:"+str(self.img))
+		lbl0 = QtGui.QLabel(self)
+		lbl0.setPixmap(pixmap)
+		#hbox.addWidget(lbl)
+		#self.setLayout(hbox)
+		lbl0.move(200,90)
+		lbl0.resize(120,70)
+		"""
+
+		pc1 = self.ping("192.168.1.19","pc1",200,90, self)
+		thread = worker("192.168.1.19","pc1",200,90)
+		self.connect(thread, thread.signal, self.testfunc)
+		thread.start()
+#25		
+		#pc1 = self.ping("192.168.0.172","pc1",200,90, self)
 		
 #24
 		pc2 = self.ping("192.168.0.127","pc2",350,90,self)
@@ -87,20 +120,42 @@ class Window(QtGui.QMainWindow):
 
 #16
 		pc11 = self.ping("192.168.0.128","pc11",350,360,self)
-		
+	
+	def testfunc(self, sigstr):
+		print ('sigstr: '+ sigstr)
+		"""
+		if sigstr == "off":
+			#img='pc0off.png'
+			self.setImage('off')
+		else:
+			#img='pc0on.png'
+			self.setImage('on')
+		#self.s.close()
+		print (self.img)
+		"""
+		self.img = sigstr
+		pixmap = QtGui.QPixmap(sigstr)
+		lbl0 = QtGui.QLabel(self.obj)
+		lbl0.setPixmap(pixmap)
+		#hbox.addWidget(lbl)
+		#self.setLayout(hbox)
+		lbl0.move(50,90)
+		lbl0.resize(120,70)
+		lbl0.show()
+
 	def ping(self,ip,name, movex , movey , obj ,status="off"):
 		self.ip = ip
 		self.name = name
 		self.obj = obj
-		self.s =  socket.socket ()
-		self.s.settimeout (0.25)
+		
 
 		self.img = str(self.name) + str(status) + '.png'
 		self.movex = movex
 		self.movey = movey
-		self.setImage('off')
-		self.setLabel()
-		self.pingPc()
+
+		#self.setImage('on')
+		#self.setLabel()
+		#self.pingPc()
 
 	def pingPc(self):
 		try:
@@ -124,8 +179,9 @@ class Window(QtGui.QMainWindow):
 
 	def setImage(self,st):
 		self.img = str(self.name) + (st) + '.png'
-	def testfunc(self, sigstr):
-		print (sigstr)
+	
+
+
 
 	def close_application(self):            # Close Conformation
 		choice = QtGui.QMessageBox.question(self, 'Exit!',
